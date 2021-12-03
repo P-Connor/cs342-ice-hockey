2d6cf0f2d9b7e87557d20f0ca7ab93b0dd84daad
Connor Popp <cmpopp1@yahoo.com>
2021-12-03 04:58:32 -0600

    Added dummy files based on previous homeworks

---
+++ image_agent/model.py
@@ -0,0 +1,33 @@
+
+import torch
+
+#TODO change all of this. This was just copied from the homework 3 solution
+class PuckLocator(torch.nn.Module):
+    class Block(torch.nn.Module):
+        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+            super().__init__()
+            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, bias=False)
+            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+            self.b1 = torch.nn.BatchNorm2d(n_output)
+            self.b2 = torch.nn.BatchNorm2d(n_output)
+            self.b3 = torch.nn.BatchNorm2d(n_output)
+            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+
+        def forward(self, x):
+            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+
+    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
+        super().__init__()
+        L = []
+        c = 3
+        for l in layers:
+            L.append(self.Block(c, l, kernel_size, 2))
+            c = l
+        self.network = torch.nn.Sequential(*L)
+        self.classifier = torch.nn.Linear(c, n_output_channels)
+
+    def forward(self, x):
+        z = self.network(x)
+        return self.classifier(z.mean(dim=[2, 3]))
